36c7022184287974f900f5d8d5e4ccfef9ae6a49
yeji <yan0.ye.zi@gmail.com>
2026-02-08 19:34:47 +0900

    scroll bug resetting

--- src/components/wedding/01-Cover.jsx
+++ src/components/wedding/01-Cover.jsx
@@ -5,6 +5,9 @@ import weddingData from "../../data/weddingData";
 export default function Cover() {
   const { cover, contacts } = weddingData;
 
+  const vh = window.innerHeight * 0.01; // innerHeight에 0.01을 곱해서 새로운 vh 단위를 구함
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
   return (
     <section className="cover snap-page">
       <div className="cover-inner">
